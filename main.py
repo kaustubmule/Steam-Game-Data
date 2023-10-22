9329d3d557fc3f59ae0b1c5300b90ca4fc43d1c6
kaustubmule <kaumule37@gmail.com>
2023-10-22 15:52:02 +0530

    gu

--- main.py
+++ main.py
@@ -1,17 +1,69 @@
 import pandas as pd
+import random
 
 games = pd.read_csv('games-features.csv')
-
 
 # print(games.head())
 
 # print(games.columns)
 
-# print(games.info())
+#print(games.info())
 
-print(games.isnull().sum())
+##Data cleaning
+null_columns = games.columns[games.isnull().any()]
+print(null_columns)
 
-num_nas = games.isna().sum().sum()
-print(num_nas)
+games.dropna(subset=[
+    'QueryName', 'SupportEmail', 'SupportURL', 'LegalNotice', 'Website',
+    'DetailedDescrip'
+],
+             inplace=True)
 
-# print(games.describe())
+#print(games)
+#null_columns = games.columns[games.isnull().any()]
+#print(null_columns)
+#print(num_nas)
+
+##Data Reduction
+#print('Before Data Reduction: \n', games)
+'''
+#Convert Generic/Categoric/Boolean to Numeric:
+games['IsFree'] = games['IsFree'].astype(int)
+for x in games.index:
+  print(games['IsFree'][x])
+
+#print('After Data Reduction: \n', games)
+games.drop(games.columns[games.dtypes == 'bool'], axis=1, inplace=True)
+print(games.columns)
+
+#Deleting the duplicates
+#games.sort_values('QueryName', inplace=True)
+games.drop_duplicates(subset='QueryName', keep=False, inplace=True)
+print(games)
+
+#Printing the updated list
+for x in games.index:
+  print(games['QueryName'][x])
+
+#transforming 0's in Req age
+'''
+cnt=0
+mean=0
+
+for i in games.index:
+  a=random.choice([0,15,18,22,25])
+  cnt+=1
+  mean+=a  
+
+mean=mean//cnt
+
+games['RequiredAge'] = games['RequiredAge'].replace(0,mean)
+#for x in games.index:
+#  print(games['RequiredAge'][x])
+'''
+games['PCRecReqsText'].fillna('Ryzen 5', inplace=True)
+for x in games.index:
+  print(games['PCRecReqsText'][x])
+
+#games.to_csv('output.csv', index=False)
+'''
